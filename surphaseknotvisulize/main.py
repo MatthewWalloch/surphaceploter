7a1e5a1250052140180b39e0cf90a65a79d9c1a0
MatthewWalloch <105795240+MatthewWalloch@users.noreply.github.com>
2023-04-11 12:50:36 -0400

    Update main.py
    fixed the plot maybe

--- surphaseknotvisulize/main.py
+++ surphaseknotvisulize/main.py
@@ -5,7 +5,7 @@ from sympy import cos, sin, solve, Symbol, Interval, solveset
 #need to have sin(sin-1(w0/kx)
 global ax
 global w
-w=0
+w=-1/4
 p = np.pi
 ax = plt.figure().add_subplot(projection='3d')
 plt.xlabel("x")
@@ -18,62 +18,95 @@ def plottangle(k,l1,l2):
     plt.show()
 
 #bridge trisection mayer and zupan
-def plotroationbad(k,l1,l2):
-    #https://www.heldermann-verlag.de/jgg/jgg01_05/jgg0404.pdf
-    rotate = []
-    theta = fsolve(sinsol, np.linspace(0, 1, 100))
-    tfirst = fsolve(trefoilx, np.linspace(0, 1, 100))
-    thetacheckdoup = []
-    tcheckdoup = []
-    for q in tfirst:
-        if trefoilx(q) < 1e-7:
-            if trefoilx(q) > -1e-7:
-                tcheckdoup.append(q)
-    tcheck = []
-    for q in tcheckdoup:
-        check = True
-        if q < 0 or q > 2*p:
+# def plotroationbad(k,l1,l2):
+#     #https://www.heldermann-verlag.de/jgg/jgg01_05/jgg0404.pdf
+#     rotate = []
+#     # theta = fsolve(sinsol, np.linspace(0, 1, 100))
+#     # tfirst = fsolve(trefoilx, np.linspace(0, 1, 100))
+#     # thetacheckdoup = []
+#     # tcheckdoup = []
+#     # for q in tfirst:
+#     #     if trefoilx(q) < 1e-7:
+#     #         if trefoilx(q) > -1e-7:
+#     #             tcheckdoup.append(q)
+#     # tcheck = []
+#     # for q in tcheckdoup:
+#     #     check = True
+#     #     if q < 0 or q > 2*p:
+#     #         continue
+#     #     for item in tcheck:
+#     #         if abs(q-item) < 1e-5:
+#     #             check = False
+#     #     if check:
+#     #         tcheck.append(q)
+#     # for q in theta:
+#     #     if np.sin(2*p*q) < 1e-7:
+#     #         if np.sin(2*p*q) > -1e-7:
+#     #             thetacheckdoup.append(q)
+#     # thetacheck = []
+#     # for q in thetacheckdoup:
+#     #     check = True
+#     #     if q < 0 or q > 2*p:
+#     #         continue
+#     #     for item in thetacheck:
+#     #         if abs(q-item) < 1e-5:
+#     #             check = False
+#     #     if check:
+#     #         thetacheck.append(q)
+#     # print(tcheck)
+#     # for i in thetacheck:
+#     #     x = []
+#     #     y = []
+#     #     z = []
+#     #     for t in range(0, 100):
+#     #         point = trefoil(t/100.0,i)
+#     #         x.append(point[0])
+#     #         y.append(point[1])
+#     #         z.append(point[2])
+#     #     ax.plot(x,y,z)
+#     # for i in tcheck:
+#     #     x = []
+#     #     y = []
+#     #     z = []
+#     #     for t in range(0, 100):
+#     #         point = trefoil(i, t / 100.0)
+#     #         x.append(point[0])
+#     #         y.append(point[1])
+#     #         z.append(point[2])
+#     #     ax.plot(x, y, z)
+#     plt.show()
+
+
+def plotrotate(k,l1,l2):
+    interval = np.linspace(0, 1, 100)
+    posx = []
+    posy = []
+    posz = []
+    negx = []
+    negy = []
+    negz = []
+    for t in interval:
+        test = trefoilx(t)
+        try:
+            arcsin = np.arcsin(w/test)
+        except:
             continue
-        for item in tcheck:
-            if abs(q-item) < 1e-5:
-                check = False
-        if check:
-            tcheck.append(q)
-    for q in theta:
-        if np.sin(2*p*q) < 1e-7:
-            if np.sin(2*p*q) > -1e-7:
-                thetacheckdoup.append(q)
-    thetacheck = []
-    for q in thetacheckdoup:
-        check = True
-        if q < 0 or q > 2*p:
-            continue
-        for item in thetacheck:
-            if abs(q-item) < 1e-5:
-                check = False
-        if check:
-            thetacheck.append(q)
-    print(tcheck)
-    for i in thetacheck:
-        x = []
-        y = []
-        z = []
-        for t in range(0, 100):
-            point = trefoil(t/100.0,i)
-            x.append(point[0])
-            y.append(point[1])
-            z.append(point[2])
-        ax.plot(x,y,z)
-    for i in tcheck:
-        x = []
-        y = []
-        z = []
-        for t in range(0, 100):
-            point = trefoil(i, t / 100.0)
-            x.append(point[0])
-            y.append(point[1])
-            z.append(point[2])
-        ax.plot(x, y, z)
+        if (w/test) > 0:
+            theta = [arcsin, p-arcsin]
+        else:
+            theta = [p-arcsin, 2*p+arcsin]
+        for the in theta:
+            pos = trefoil(t, the)
+            if pos[0]>0:
+                posx.append(pos[0])
+                posy.append(pos[1])
+                posz.append(pos[2])
+            else:
+                negx.append(pos[0])
+                negy.append(pos[1])
+                negz.append(pos[2])
+    ax.plot(posx,posy,posz)
+    ax.plot(negx,negy,negz)
     plt.show()
 
 
@@ -81,7 +114,7 @@ def plotroationbad(k,l1,l2):
 def sinsol(t):
     return np.sin(2*p*t)-w
 def trefoilx(t):
-    return (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) -4 - w)
+    return (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) -4)
 
 
 def trefoil(t, theta):
@@ -91,12 +124,12 @@ def trefoil(t, theta):
     k0 = (np.cos(4 * p * start) * (2+np.cos(2*p*start))-4, np.sin(4 * p * start) * (2 + np.cos(6 * p * start)), np.sin(6 * p * start), 0)
     k1 = (np.cos(4 * p * stop) * (2+np.cos(2*p*stop))-4, np.sin(4 * p * stop) * (2 + np.cos(6 * p * stop)), np.sin(6 * p * stop), 0)
     if t <=start:
-        return [np.cos(theta * 2*p) * 12 * k0[0]*t, 12 * k0[0]*t,  k0[2]*12*t+(1-12*t)*-1, np.sin(theta * 2*p)* 12 * k0[0]*t]
+        return [np.cos(theta) * 12 * k0[0]*t, 12 * k0[0]*t,  k0[2]*12*t+(1-12*t)*-1, np.sin(theta)* 12 * k0[0]*t]
     elif t >= stop:
-        return [np.cos(theta * 2 * p) *12 * k1[0]*(t-stop), 12 * k1[1]*(t-stop), k1[2]*12 *(t-stop)+(1-12 * t)*1, np.sin(theta * 2*p)* 12 * k1[0]*(t-stop) ]
+        return [np.cos(theta) *12 * k1[0]*(t-stop), 12 * k1[1]*(t-stop), k1[2]*12 *(t-stop)+(1-12 * t)*1, np.sin(theta)* 12 * k1[0]*(t-stop) ]
     else:
-        return [np.cos(theta * 2*p) * (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) - 4), np.sin(4 * p * t) * (2 + np.cos(6 * p * t)),
-         np.sin(6 * p * t), np.sin(theta * 2*p)* (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) - 4) ]
+        return [np.cos(theta) * (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) - 4), np.sin(4 * p * t) * (2 + np.cos(6 * p * t)),
+         np.sin(6 * p * t), np.sin(theta)* (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) - 4) ]
 
 
 
@@ -109,4 +142,4 @@ t2 = np.linspace(0,1, 100)
 k = ((np.cos(4*p*t)*(2+np.cos(2*p*t))-4), np.sin(4*p*t)*(2+np.cos(6*p*t)), np.sin(6*p*t), 0)
 r = (k[0][-1]*t1, k[1][-1]*t1, (k[2][-1]*t1+(1-t1)*-1), 0)
 s = (k[0][0]*t1, k[1][0]*t1, (k[2][0]*t1+(1-t1)*1), 0)
-plotroationbad(k,r,s)
+plotrotate(k,r,s)
