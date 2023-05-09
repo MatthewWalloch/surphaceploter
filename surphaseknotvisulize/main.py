b1a9cb0634fc2257d6c22493c164c86412a1bd47
MatthewWalloch <105795240+MatthewWalloch@users.noreply.github.com>
2023-05-09 15:43:59 -0400

    update

--- surphaseknotvisulize/main.py
+++ surphaseknotvisulize/main.py
@@ -15,7 +15,7 @@ app = dash.Dash()
 
 def plotrotate(w):
     # we first create a line space enviormnet, ie 100 points evenly spaced on 0,1
-    interval = np.linspace(0, 1, 500)
+    interval = np.linspace(0, .9999, 500)
     # we need a posijtive and a negative version for each as the inverse sin
     posx = []
     posy = []
@@ -31,10 +31,9 @@ def plotrotate(w):
         except:
             continue
         if (w/test) > 0:
-            theta = [arcsin, p-arcsin]
+            theta = [arcsin]#, p-arcsin]
         else:
-            theta = [p-arcsin, 2*p+arcsin]
-
+            theta = [p-arcsin]#, 2*p+arcsin]
             # evaluate the rotation and seperate into a positve and a negative
         for the in theta:
             pos = trefoil(t, the)
@@ -62,77 +61,11 @@ def plotrotate(w):
                     y.append(bady[i])
                     z.append(badz[i])
     # recreates lines that attaceh the tangle to the zy axis
-    xline = []
-    yline = []
-    zline = []
-    for i in range(1, len(x)):
-        dist = np.sqrt((x[i]-x[i-1])**2+(y[i]-y[i-1])**2+(z[i]-z[i-1])**2)
-        if dist > 1:
-            if x[i] > 0:
-                xline.append(x[i])
-                yline.append(y[i])
-                zline.append(z[i])
-            if x[i-1] > 0:
-                xline.append(x[i - 1])
-                yline.append(y[i - 1])
-                zline.append(z[i - 1])
-    #the new versions of the variables will try to be
-    newx=[[x[0]]]
-    newy=[[y[0]]]
-    newz=[[z[0]]]
-    n = 0
-    placeholder = 0
-    # adds lines to x, y and z lists
-    for i in range(len(xline)):
-        for q in range(0, 10):
-            x.append(xline[i] + (1 - q * 1e-1) * (-2 * xline[i]))
-            y.append(yline[i])
-            z.append(zline[i])
-    # we will establish a range for the x and z values to better establish if the curves are disjoint
-    xrange = max(x)-min(x)
-    zrange = max(z)-min(z)
-    # this loop will add to newx, newy, newz the loops, it will check if there is jump, if there it it will make a new loop
-    for i in range(1,len(x)):
-        distance = {}
-        for j in range(len(x)):
-            distance[np.sqrt(.9*(newx[n][i-1-placeholder] - x[j]) ** 2 + (newy[n][i-1-placeholder] - y[j]) ** 2 + (newz[n][i-1-placeholder] - z[j]) ** 2)]=(x[j],y[j],z[j])
-        key = min(distance.keys())
-        # this is a checks if the minimum key, ie distance to nearest point and checks how far each jump is, if
-        # it a significant jump in x and z relative to the size of the link then it will make a new curve rather
-        # than put all the points in one big curve
-        if (key > .1*xrange) and (np.absolute(newz[n][i-1-placeholder]-distance[key][2])>.07*zrange):
-            n += 1
-            placeholder = i
-            newx.append([x[0]])
-            newy.append([y[0]])
-            newz.append([z[0]])
-        else:
-            newx[n].append(distance[key][0])
-            newy[n].append(distance[key][1])
-            newz[n].append(distance[key][2])
-            x.remove(distance[key][0])
-            y.remove(distance[key][1])
-            z.remove(distance[key][2])
-    newnewx = []
-    newnewy = []
-    newnewz = []
-    # you can just add a member of a list to the list itself, or else you get a recursive list so this was the best way
-    # i found to close the curve point list
-    for i in range(len(newx)):
-        newnewx.append([newx[i][0]])
-        newnewy.append([newy[i][0]])
-        newnewz.append([newz[i][0]])
-        for j in range(1, len(newx[i])):
-            newnewx[i].append(newx[i][j])
-            newnewy[i].append(newy[i][j])
-            newnewz[i].append(newz[i][j])
-        newnewx[i].append(newx[i][0])
-        newnewy[i].append(newy[i][0])
-        newnewz[i].append(newz[i][0])
     # adds the points to the graph
-    fig = go.Figure(data=go.Scatter3d(x=newnewx[0], y=newnewy[0],z=newnewz[0], mode='lines'))
-    for i in range(1,len(newnewx)):
-        fig.add_trace(go.Scatter3d(x=newnewx[i], y=newnewy[i],z=newnewz[i], mode='lines'))
+    fig = go.Figure(data=go.Scatter3d(x=x, y=y, z=z, mode='lines'))
+    # fig = go.Figure(data=go.Scatter3d(x=x[0], y=newnewy[0],z=newnewz[0], mode='lines'))
+    # for i in range(1,len(newnewx)):
+    #     fig.add_trace(go.Scatter3d(x=newnewx[i], y=newnewy[i],z=newnewz[i], mode='lines'))
     # makes sure lines look pretty
     fig.update_traces(
         line=dict(
@@ -146,29 +79,38 @@ def plotrotate(w):
             yaxis=dict(nticks=10, range=[-10, 10], ),
             zaxis=dict(nticks=10, range=[-10, 10], ), ),
         scene_aspectmode='cube',
-        uirevision=False)
+        uirevision=1)
     return fig
     #plt.show()
 
 
 # the x of trefoil parameterization used to determine the invese sin
 def trefoilx(t):
-    return (np.cos(6 * p * t) * (2 + np.cos(3 * p * t)) -4)
+    return (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) -4)
 
 # paramterization of the trefoil between 1/12 and 11/12
 def trefoil(t, theta):
     start = 1.0 / 12
     stop = 11.0 / 12
-    if t <=start:
-        return [0,0,0,0]
-    elif t >= stop:
-        return [0,0,0,0]
+    k0 = (np.cos(4 * p * start) * (2 + np.cos(2 * p * start)) - 4, np.sin(4 * p * start) * (2 + np.cos(6 * p * start)),
+          np.sin(6 * p * start), 0)
+    k1 = (np.cos(4 * p * stop) * (2 + np.cos(2 * p * stop)) - 4, np.sin(4 * p * stop) * (2 + np.cos(6 * p * stop)),
+          np.sin(6 * p * stop), 0)
+    if 0 <= t <= start:
+        return [np.cos(theta) * k0[0] * (12 * t),
+                k0[1] * (12 * t) + (1 - 12 * t),
+                k0[2] * (12 * t),
+                np.sin(theta) * k0[0] * (12 * t)]
+    elif 1 > t >= stop:
+        return [np.cos(theta) * k1[0] * (1 - 12 * (t - stop)),
+                k1[1] * (1 - 12 * (t - stop)) + (12 * (t - stop)) * -1,
+                k1[2] * (1 - 12 * (t - stop)),
+                np.sin(theta) * k1[0] * (1 - 12 * (t - stop))]
     else:
-        return [np.cos(theta) * (np.cos(6 * p * t) * (2 + np.cos(3* p * t)) - 4),
-                np.sin(6 * p * t) * (2 + np.cos(10 * p * t)),
-                np.sin(10 * p * t),
-                np.sin(theta)* (np.cos(6 * p * t) * (2 + np.cos(3 * p * t)) - 4) ]
-
+        return [np.cos(theta) * (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) - 4),
+                np.sin(4 * p * t) * (2 + np.cos(6 * p * t)),
+                np.sin(6 * p * t),
+                np.sin(theta) * (np.cos(4 * p * t) * (2 + np.cos(2 * p * t)) - 4)]
 # makes the dash enviorment work with slider and what not
 app.layout = html.Div(id='parent', children=[
     html.H1(id='H1', children='Visualizing the spun trefoil', style={'textAlign': 'center', \
